a30f90aa3f0656f3a772e7f9268ee187c41d56ec
eferreccio <estebanferreccio@gmail.con>
2021-10-22 11:53:04 -0300

    destination public/img/products

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/img");
+    cb(null, "public/img/products");
   },
   filename: function (req, file, cb) {
     cb(
