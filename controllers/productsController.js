2b73157b154a47d301f8c2dea9b0042064b76a59
nicolasenrique <nicolas.enrique56@gmail.com>
2021-10-22 11:02:49 -0300

    adding new ver

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,13 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const productsFilePath = path.join(__dirname, "../data/productos.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+// Show all products
 const controlProducts = {
-    list: function(req,res){
-        res.render('productList');        
-    },
-    detail: function(req,res){
-        res.render('productDescription');        
-    },
-    admin: function(req,res){
-        res.render('product_admin');
-    } 
-}
+  list: function (req, res) {
+    res.render("productList", { products });
+  },
+  // Shows one product
+  detail: function (req, res) {
+    let idProducto = req.params.id - 1;
+    let selectedProduct = products[idProducto];
+    res.render("productDescription", { selectedProduct });
+  },
+  admin: function (req, res) {
+    res.render("product_admin");
+  },
+};
 
 module.exports = controlProducts;
--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/productsController");
 
-router.get("/list", productsController.list);
-router.get('/description', productsController.detail);
-router.get('/admin', productsController.admin);
+router.get("/list", productsController.list); // Sugerencia Nico router.get("/", productsController.list);
+router.get("/description/:id/", productsController.detail);
+router.get("/admin", productsController.admin);
 
 module.exports = router;
